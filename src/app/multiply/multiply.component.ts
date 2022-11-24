8ff56c7b3bafe40b095d4c52a21ae10db6c9d798
ShubhamKinekar <101723493+ShubhamKinekar@users.noreply.github.com>
2022-11-24 16:36:13 +0530

    add multiply

---
+++ src/app/multiply/multiply.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multiply',
+  templateUrl: './multiply.component.html',
+  styleUrls: ['./multiply.component.css']
+})
+export class MultiplyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
